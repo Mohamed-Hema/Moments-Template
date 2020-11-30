c78cd1c9b6eca31077e70575451a933aff898dd6
Mohamed-Hema <mohamedhema2017@gmail.com>
2020-11-30 16:11:58 +0200

    added carousels

--- scripts/main.js
+++ scripts/main.js
@@ -11,54 +11,92 @@ menu.addEventListener('click', function() {
     header.classList.toggle('menu-open')
 });
 
+// First Carousel Section
 
-// Start Carousel Section
+(function() {
+  "use strict";
 
-$(document).ready(function(){
-    //Scroll to Top
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
-        } else {
-            $('#back-to-top').fadeOut();
-        }
-    });
-
-
+  var carousel = document.getElementsByClassName('carousel')[0],
+      slider = carousel.getElementsByClassName('carousel__slider')[0],
+      items = carousel.getElementsByClassName('carousel__slider__item'),
+      prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
+      nextBtn = carousel.getElementsByClassName('carousel__next')[0];
+  
+  var width, height, totalWidth, margin = 20,
+      currIndex = 0,
+      interval, intervalTime = 4000;
+  
+  function init() {
+      resize();
+      move(Math.floor(items.length / 2));
+      bindEvents();
     
-    // scroll slides on mouse scroll 
-    $('#myCarousel').bind('mousewheel DOMMouseScroll', function(e){
+      timer();
+  }
+  
+  function resize() {
+      width = Math.max(window.innerWidth * .25, 275),
+      height = window.innerHeight * .5,
+      totalWidth = width * items.length;
     
-            if(e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-                $(this).carousel('prev');
-                
-                
-            }
-            else{
-                $(this).carousel('next');
-                
-            }
-        });
+      slider.style.width = totalWidth + "px";
     
-    //scroll slides on swipe for touch enabled devices  
-         $("#myCarousel").on("touchstart", function(event){
-     
-            var yClick = event.originalEvent.touches[0].pageY;
-            $(this).one("touchmove", function(event){
+      for(var i = 0; i < items.length; i++) {
+          let item = items[i];
+          item.style.width = (width - (margin * 2)) + "px";
+          item.style.height = height + "px";
+      }
+  }
+  
+  function move(index) {
     
-            var yMove = event.originalEvent.touches[0].pageY;
-            if( Math.floor(yClick - yMove) > 1 ){
-                $(".carousel").carousel('next');
-            }
-            else if( Math.floor(yClick - yMove) < -1 ){
-                $(".carousel").carousel('prev');
-            }
-        });
-        $(".carousel").on("touchend", function(){
-                $(this).off("touchmove");
-        });
-    });
-        
-    });
+      if(index < 1) index = items.length;
+      if(index > items.length) index = 1;
+      currIndex = index;
+    
+      for(var i = 0; i < items.length; i++) {
+          let item = items[i],
+              box = item.getElementsByClassName('item__3d-frame')[0];
+          if(i == (index - 1)) {
+              item.classList.add('carousel__slider__item--active');
+              box.style.transform = "perspective(1200px)"; 
+          } else {
+            item.classList.remove('carousel__slider__item--active');
+              box.style.transform = "perspective(1200px) rotateY(" + (i < (index - 1) ? 40 : -40) + "deg)";
+          }
+      }
+    
+      slider.style.transform = "translate3d(" + ((index * -width) + (width / 2) + window.innerWidth / 2) + "px, 0, 0)";
+  }
+  
+  function timer() {
+      clearInterval(interval);    
+      interval = setInterval(() => {
+        move(++currIndex);
+      }, intervalTime);    
+  }
+  
+  function prev() {
+    move(--currIndex);
+    timer();
+  }
+  
+  function next() {
+    move(++currIndex);    
+    timer();
+  }
+  
+  
+  function bindEvents() {
+      window.onresize = resize;
+      prevBtn.addEventListener('click', () => { prev(); });
+      nextBtn.addEventListener('click', () => { next(); });    
+  }
 
-// End Carousel Section
+
+
+
+  
+  init();
+  
+})();
